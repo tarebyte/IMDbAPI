11427319339677b5c105c1dfa2af8cc5c4963149
Mark Tareshawty <tarebyte@gmail.com>
2013-03-21 21:37:21 -0400

    added locale files and fixed version

--- lib/imdbapi.rb
+++ lib/imdbapi.rb
@@ -1,5 +1,12 @@
-require "imdbapi/version"
+require_relative "imdbapi/version"
+
+require 'active_support/core_ext/module/delegation'
+require 'json'
+require 'net/http'
+
+require_relative 'imdbapi/movie.rb'
+require_relative 'imdbapi/search.rb'
+require_relative 'imdbapi/top_250.rb'
 
 module Imdbapi
-  # Your code goes here...
 end
